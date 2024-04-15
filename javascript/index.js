9538c5522f375bad0e3c8ac80800dae3bd2622fd
kaylavera99 <kaylavd99@gmail.com>
2024-04-14 23:20:32 -0400

    Week 5 Assignment
    HTML & CSS Grid with Aria Attributes

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to close the navbar collapse
+    function closeNavbarCollapse() {
+      const navbarCollapse = document.querySelector('.navbar-collapse');
+      navbarCollapse.classList.remove('show');
+  
+      const backdrop = document.querySelector('.navbar-backdrop');
+      backdrop.style.display = 'none';
+    }
+  
+    // Close the navbar collapse when clicking outside of it
+    document.addEventListener('click', function (event) {
+      const navbarToggler = event.target.closest('.navbar-toggler');
+      const navbarCollapse = event.target.closest('.navbar-collapse');
+      if (!navbarToggler && !navbarCollapse) {
+        closeNavbarCollapse();
+      }
+    });
+  
+    // Close the navbar collapse when clicking on the backdrop
+    const backdrop = document.querySelector('.navbar-backdrop');
+    backdrop.addEventListener('click', function () {
+      closeNavbarCollapse();
+    });
+  });
+  
