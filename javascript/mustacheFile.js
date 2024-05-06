016754be39005c198bcd77e28ea1e4af63f73777
kaylavera99 <kaylavd99@gmail.com>
2024-05-05 23:26:49 -0400

    Final Project

--- javascript/mustacheFile.js
+++ javascript/mustacheFile.js
@@ -6,44 +6,61 @@
         
     var testimonialData = [
         {
-        text: "I can't express enough how much your product has changed my life! As someone with food allergies, dining out used to be a constant source of stress and anxiety for me",
+        text: "Thanks to WeEat, dining out is no longer a source of anxiety for me. I used to worry constantly about accidental exposure to allergens, but now I can enjoy meals with friends and family without fear. The app has truly made a positive impact on my life, making dining experiences more inclusive and allergy-aware. Thank you, WeEat!",
           author: "Kathleen G.",
-          source: "Google"
+          source: "Google",
+          pathsource: "socials/google.png",
+          pathauthor: "users/people_1.png"
+
         },
         {
           text: "...I often felt left out or misunderstood, and it was challenging to find places that could accommodate my dietary needs",
           author: "Sarah M.",
-          source: "Yelp"
+          source: "Yelp",
+          pathsource: "socials/yelp.png",
+          pathauthor: "users/people_2.png"
+
         },
         {
-          text: " I no longer have to worry about accidental exposure to allergens or feeling excluded from social gatherings",
+         text: "WeEat has transformed the way I dine out. Previously, I felt excluded from social gatherings because of my allergies, but now I can confidently join in the fun. Dining out has become a joyous experience filled with delicious meals and cherished moments. I'm grateful to WeEat for making the world a more inclusive place.",
           author: "Megan D.",
-          source: "Google"
+          source: "Google",
+          pathsource: "socials/google.png",
+          pathauthor: "users/people_3.png"
         },
         {
-          text: " Thank you for making the world a more inclusive and allergy-aware place!",
+          text: "Since discovering WeEat, I no longer feel like an outsider when dining out. The app's allergy-aware features have made me feel safe and included, allowing me to enjoy meals with friends and family without worry. WeEat has truly improved my dining experience and enriched my life. Thank you for creating such a valuable tool",
           author: "Conor J.",
-          source: "App Store"
+          source: "App Store",
+          pathsource: "socials/appstore.png",
+          pathauthor: "users/people_4.png"
         }
-    ]
+    ];
 
 
     var template = $("#testimonial-template").html();
+    Mustache.parse(template);
+    var rendered = "";
 
-    for (var i = 0; i <testimonialData.length; i++) {
-        var rendered = Mustache.render(template, testimonialData[i]);
-        $("#testimonials-container").html(rendered);
+
+    var fullPage = $("#testimonials-container").hasClass("full-page");
+
+    if (fullPage) {
+      for (var i = 0; i <testimonialData.length; i++) {
+        rendered += Mustache.render(template, testimonialData[i]);
+        
     }
 
-    // Define your Mustache template
-    
+    } else {
+      rendered += Mustache.render(template, testimonialData[0]);
+    }
 
-    // Render the template with the testimonial data
-  
 
-    // Insert the rendered HTML into the testimonials container
+    $("#testimonials-container").html(rendered);
+
+
     
- })
+ });
  
 
 
